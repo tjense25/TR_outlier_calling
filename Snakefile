a57fdf8fc0c35e66593434df870e6c67e82f5a50
tjense25 <tannerj@stanford.edu>
2024-02-07 11:13:43 -0800

    updated code to call outliers from expansion hunter input

--- Snakefile
+++ Snakefile
@@ -1,10 +1,13 @@
 
 VNTR_INPUT="/oak/stanford/groups/euan/projects/tannerj/ALNG_DB/VNTR.vamos.cohort_combined.ALNG_DB.AF_annotated.sorted.txt"
+VNTR_INPUT_EH="/oak/stanford/groups/euan/projects/tannerj/short_read_SV_workflow/results/expansionhunter/UDN.short_read.expansionhunter.tandemrepeats.tsv"
+
 k = [10]
 chroms = ["chr%d" % i for i in range(1,23)] + ['chrX', 'chrY']
 rule all:
     input:
-      expand("VNTR.mean_neighbor_distance.kneighbors_{k}.extreme_outliers.csv", k=k)
+      expand("VNTR.mean_neighbor_distance.kneighbors_{k}.extreme_outliers.csv", k=k),
+      "VNTR.EH.mean_neighbor_distance.kneighbors_25.extreme_outliers.csv"
 
 rule VNTR_outlier:
     threads: 16
@@ -42,6 +45,40 @@ rule concat_results:
     cat {input} | grep -E -v "^VNTR_ID" >> {output}
   """
 
-#rule convert_to_vcf:
-#  ""
-#
+rule VNTR_outlier_EH:
+    threads: 16
+    resources:
+        mem=128,
+        time=12
+    input:
+      VNTR_INPUT_EH
+    params: 
+        st_catalog = "/oak/stanford/groups/smontgom/tannerj/UDN_LRS_watershed/repeat_expansion_testing/1kg.gt.hist.tsv.gz",
+        script = "call_VNTR_outliers.expansion_hunter.R",
+        tmpdir = "/tmp/tannerj/VNTR_outliers.chrom_{chr}.k_{k}"
+
+    output:
+        temp("VNTR.EH.mean_neighbor_distance.kneighbors_{k}.extreme_outliers.chrom_{chr}.csv")
+    conda: 'r'
+    shell: """
+        mkdir -p {params.tmpdir}
+        
+        head -1 {input} > {params.tmpdir}/input_VNTR.tsv
+        awk "\$3==\\"{wildcards.chr}\\"" {input} >> {params.tmpdir}/input_VNTR.tsv
+        Rscript {params.script} {params.tmpdir}/input_VNTR.tsv {output} {params.str_catalog} {wildcards.k}
+        rm -rf {params.tmpdir}
+    """
+
+rule concat_results_EH:
+  threads: 1 
+  resources:
+    mem=24,
+    time=4
+  input:
+    expand("VNTR.EH.mean_neighbor_distance.kneighbors_{{k}}.extreme_outliers.chrom_{chr}.csv", chr=chroms),
+  output:
+    "VNTR.EH.mean_neighbor_distance.kneighbors_{k}.extreme_outliers.csv"
+  shell: """
+    head -1 {input[0]} > {output}
+    cat {input} | grep -E -v "^VNTR_ID" >> {output}
+  """
---
+++ call_VNTR_outliers.expansion_hunter.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(data.table)
+library(cowplot)
+library(parallel)
+library(pbmcapply)
+
+args = commandArgs(trailingOnly=TRUE)
+vntr_file = args[1]
+out_file=args[2]
+ref_TR_catalog=args[3]
+k=as.numeric(args[4])
+
+calc_VNTR_outliers <- function(samples, haplotypes, repeat_length, VNTR_ID, k=25) {
+    TR_catalog <- ref_TR_catalog[ref_TR_catalog$VariantId %in% VNTR_ID, ]
+    catalog_lengths <- as.numeric(str_split_1(TR_catalog$RepeatNumbers,','))
+    catalog_counts <- as.numeric(str_split_1(TR_catalog$AlleleCounts, ','))
+    combined_lengths <- c(repeat_length, rep(catalog_lengths, catalog_counts))
+    sigma <- max(sqrt(mean(combined_lengths,na.rm=T)),sd(combined_lengths,na.rm=T))
+    D <- as.matrix(dist(combined_lengths,upper=T,diag=T))
+    #mean_neighbor_distance <- apply(D,1,function(x) mean(x[order(x)[2:(1+k)]]))
+    mean_neighbor_distance <- apply(D[1:length(samples),],1,function(x) mean(x[order(x)[2:(1+k)]]))
+    outlier = mean_neighbor_distance > 2*sigma & (repeat_length > quantile(combined_lengths,.99) | repeat_length < quantile(combined_lengths,.01))
+    outlier_sd = mean_neighbor_distance/sigma
+    return(data.frame(samples,haplotypes,repeat_length,rank=rank(-combined_lengths)[1:repeat_length],mnd=mean_neighbor_distance, outlier, mnd_sigmas=outlier_sd, repeat_length_mean=mean(combined_lengths), repeat_length_sd=sd(combined_lengths)))
+}
+
+vntrs <- fread(vntr_file)
+colnames(vntrs) <- c("chrom", "start","end","VNTR_ID","repeat_unit","sample","haplotype","size","size_distribution")
+ref_TR_catalog = fread(ref_TR_catalog)
+vntrs <- vntrs[vntrs$VNTR_ID %in% ref_TR_catalog$VariantId, ] #subset to reference VNTRs
+ref_TR_catalog <- ref_TR_catalog[ref_TR_catalog$VariantId %in% vntrs$VNTR_ID,] #subset ref catalog to same VNTRS as main file
+
+vntr_unique_ids <- unique(vntrs$VNTR_ID)
+chunked_ids <- split(vntr_unique_ids,ceiling(seq_along(vntr_unique_ids)/1000))
+chunked_ids <- chunked_ids[1:16]
+outliers <- Reduce(rbind,pbmclapply(chunked_ids, function(x) vntrs %>% filter(VNTR_ID %in% x) %>% group_by(VNTR_ID) %>% reframe(calc_VNTR_outliers(sample, haplotype, size, VNTR_ID, k=k))))
+outliers$mnd
+fwrite(outliers, out_file)
+
